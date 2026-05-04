9cf6ec8b7b867fcdfa1f347f756ede829a1a964c
Tairitsu <trgtrn222@gmail.com>
2026-05-05 01:16:29 +0700

    chore: setup mysql service with docker-compose

---
+++ src/main/java/com/tairitsu/driverincome/DriverincomeApplication.java
@@ -0,0 +1,13 @@
+package com.tairitsu.driverincome;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DriverincomeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DriverincomeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tairitsu/driverincome/DriverincomeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tairitsu.driverincome;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DriverincomeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
