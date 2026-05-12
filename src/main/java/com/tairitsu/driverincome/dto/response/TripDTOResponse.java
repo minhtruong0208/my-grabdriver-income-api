88135a07bea56034028b2010b57557306029aa24
Tairitsu <trgtrn222@gmail.com>
2026-05-13 00:51:36 +0700

    fix: allow zero value for trip tip

--- src/main/java/com/tairitsu/driverincome/dto/response/TripDTOResponse.java
+++ src/main/java/com/tairitsu/driverincome/dto/response/TripDTOResponse.java
@@ -3,6 +3,7 @@ package com.tairitsu.driverincome.dto.response;
 import com.tairitsu.driverincome.entity.TripType;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Positive;
+import jakarta.validation.constraints.PositiveOrZero;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -19,7 +20,7 @@ public class TripDTOResponse {
     @NotNull(message = "Thu nhập ròng không được để trống")
     @Positive(message = "Thu nhập ròng phải > 0")
     private BigDecimal netIncome;
-    @Positive(message = "Tiền bo phải > 0")
+    @PositiveOrZero(message = "Tiền bo không được âm")
     private BigDecimal tip;
     @NotNull
     @Positive
